74887b0686dfece20cdfa21929245123fcfe4785
shifvb <shifvb@gmail.com>
2017-03-23 16:58:42 +0800

    change the my_imread method by using PIL rather than cv2, comment the deprecated method

--- my_imread.py
+++ my_imread.py
@@ -1,11 +1,17 @@
-import cv2
+from PIL import Image
+import numpy as np
 
 
-def my_imread(path):
-    arr = cv2.imread(path)
-    for i in range(arr.shape[0]):  # change BGR to RGB image
-        for j in range(arr.shape[1]):
-            t = arr[i][j][2]
-            arr[i][j][2] = arr[i][j][0]
-            arr[i][j][0] = t
-    return arr
+# def my_imread_deprecated(path):
+#     import cv2
+#     arr = cv2.imread(path)
+#     for i in range(arr.shape[0]):  # change BGR to RGB image
+#         for j in range(arr.shape[1]):
+#             t = arr[i][j][2]
+#             arr[i][j][2] = arr[i][j][0]
+#             arr[i][j][0] = t
+#     return arr
+
+
+def my_imread(path: str):
+    return np.asarray(Image.open(path), dtype=np.uint8)
